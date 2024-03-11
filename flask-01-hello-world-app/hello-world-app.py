dc8d0220344b088306de15451ffd151d62b8ea48
furkan can <furkancan1995dr@gmail.com>
2024-03-11 23:37:20 +0300

    second

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,24 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "hello world clarusway-17"
 
+@app.route("/second")
+def head_second():
+    return "2. sayfa helelele"
 
+@app.route("/third")
+def head_uc():
+    return "3. sayfa heleddıufhewıbjhele"
 
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"bu sayfanın id'si {id}"
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    # app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
